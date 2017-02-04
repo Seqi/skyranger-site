876ce11bed750f047957d15d50e2fc9b5ed4dad1
seqi <tomseqi@live.co.uk>
2017-02-04 15:48:03 +0000

    Bin file parser started, reading in the buffer from the file

---
+++ app.js
@@ -0,0 +1,7 @@
+var file = "./template/Demo.bin";
+// Instantiate the xcom parser with the file
+var xcomParser = require('./utility/parsesoldiers')(file);
+
+xcomParser.parse();
+
+xcomParser.verifyFile();
---
+++ utility/parsesoldiers.js
@@ -0,0 +1,34 @@
+// NB: To aid with the learning process of node/js, maybe seemingly obvious
+// things are commented. Said things may be incorrect. If so, correct me!
+
+// The use of strict ensures that xcomPoolParser is instantiated rather than
+// referenced. If references, this.offset creates a global variable. If
+// instantiated, it creates a variable in the scope of the object only.
+"use strict";
+
+var fs = require('fs');
+
+// This is the public object that is passed back, exposing any functions
+// the caller might need.
+function xcomPoolParser(path){
+  this.offset = 0;
+  this.buffer = [];
+
+  // TODO: Replace public functions with actual code
+  this.verifyFile = function(){
+    console.log("verify");
+  }
+
+  this.parse = function(){
+    this.buffer = load(path);
+    console.log(this.buffer);
+  }
+}
+
+function load(path){
+  return fs.readFileSync(path);
+}
+
+module.exports = function(path){
+  return new xcomPoolParser(path);
+}
