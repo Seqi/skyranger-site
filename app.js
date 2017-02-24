d14a50359ae98ce53613beea9a2ff139ee9e6f10
seqi <tomseqi@live.co.uk>
2017-02-24 19:53:53 +0000

    Outputs whole object rather than just soldiers

--- app.js
+++ app.js
@@ -7,8 +7,6 @@
 
 var file = "./template/Demo.bin";
 
-<<<<<<< df164c590521e39b2cf7099646902921b1914e96
-var soldierWriter = require('./utility/soldier-writer');
 var testPerson = [
   {
     strFirstName: "Phil2",
@@ -17,31 +15,12 @@ var testPerson = [
     BackgroundText: "No one knows anything about Phil because he tweets once a week. Spends most of his time ERPing on Final Fantasy XIV. Seen so much Miqo'te porn that he's slowly moving into furry territory."
   }
 ];
-=======
-var singleSoldier = soldierReader(fileSingleSoldier).getSoldiers();
-console.log("Retrieved " + singleSoldier.soldiers.length + " soldiers.");
-console.log(JSON.stringify(singleSoldier, null, 4));
->>>>>>> Reader now returns object rather than output
 
-<<<<<<< c5912b3f593fa36aa7a71c3163d0ef4b5337ad83
+var soldierWriter = require('./utility/soldier-writer');
 var newSoldier = soldierWriter(testPerson)
-=======
-console.log("----------------------------------");
->>>>>>> Soldiers property intiialised with header. Outputting both tests.
 
-<<<<<<< df164c590521e39b2cf7099646902921b1914e96
 // Read the new soldier back in to ensure it parses correctly
 var reader = require('./utility/soldier-reader')(newSoldier);
 var output = reader.getSoldiers();
 
 console.log(JSON.stringify(output, null, 4));
-=======
-var twoSoldiers = soldierReader(fileTwoSoldiers).getSoldiers();
-<<<<<<< c5912b3f593fa36aa7a71c3163d0ef4b5337ad83
-//console.log("Retrieved " + twoSoldiers.soldiers.length + " soldiers.");
-//console.log(JSON.stringify(twoSoldiers.soldiers, null, 4));
->>>>>>> Reader now returns object rather than output
-=======
-console.log("Retrieved " + twoSoldiers.soldiers.length + " soldiers.");
-console.log(JSON.stringify(twoSoldiers.soldiers, null, 4));
->>>>>>> Soldiers property intiialised with header. Outputting both tests.
