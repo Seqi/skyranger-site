8b2df6cc7c016bd1492589feacb6590d3667ebe0
seqi <tomseqi@live.co.uk>
2017-03-08 21:00:03 +0000

    Created public folder. Text data posting and parsing

--- site/app.js
+++ site/app.js
@@ -1,4 +1,7 @@
-var app = require('express')();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
 
 var server = app.listen(8080, function onSuccessListen(){
   var host = server.address().address;
@@ -7,47 +10,69 @@ var server = app.listen(8080, function onSuccessListen(){
   console.log("Listening on %s:%s", host, port);
 })
 
+// Parses if data is sent as JSON
+app.use(bodyParser.json());
+
+// Parses if data is sent as a query string
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Loads in public resources
+app.use(express.static(__dirname + "/public/styles"));
+app.use(express.static(__dirname + "/public/scripts"));
+
+
 app.get('/', function(req, res){
-  res.writeHead(200, { "Content-Type": "text/html"});
-
-  res.write("Here is my blank XCOM website thing which hasn't been written yet");
-  res.end();
+  writePageToOutput(res);
 });
 
-app.get('/read', function(req, res){
-
-  var t = testSoldiers();
-  res.setHeader("Content-disposition", "attachment; filename=Custom.bin");
-  res.setHeader("Content-type", "application/octet-stream");
-  res.writeHead(200, { "Content-Type": "text/html"});
-
-  res.write(t);
-  res.end();
-});
-
-function testSoldiers(){
-  var soldierWriter = require('../api/src/soldier-writer');
-  var reader = require('../api/src/soldier-reader');
-
-  var testPerson = [];
-
-  var firstNames = ["Oakley", "Charlie", "Casey", "Jessie", "Skyler"];
-  var lastNames = ["Smith", "Adams", "Jackson", "Lowell", "Morgan"];
-
-  var soldiersToCreate = process.argv[2] || 1;
-  for(var i = 0; i < soldiersToCreate; i++){
-    testPerson.push({
-      strFirstName: firstNames[getRandomNumber(firstNames.length)],
-      strNickName: "'" + i + "'",
-      strLastName: lastNames[getRandomNumber(lastNames.length)],
-      iGender: getRandomNumber(2) + 1,
-      BackgroundText: "wew"
-    });
+app.post('/', function(req, res){
+  // Split the names up
+  if (!req.body){
+    return;
   }
 
-  return soldierWriter(testPerson);
+  var soldiers = [];
+  var lines = req.body.inputText.split("\n");
+
+  for(var i = 0; i < lines.length; i++){
+    var parsedName = parseName(lines[i]);
+    if (!parsedName){
+      return console.log("Bad name: " + lines[i]);
+    }
+    soldiers.push(parsedName);
+  }
+
+  console.log(soldiers);
+
+  writePageToOutput(res);
+});
+
+function parseName(name){
+  var segments = name.trim().split(' ');
+  var name = {};
+
+  if (segments.length == 1){
+    return { firstName: segments[0] };
+  }
+  else if (segments.length == 2){
+    return { firstName: segments[0], lastName: segments[1] };
+  }
+  else if (segments.length == 3){
+    return { firstName: segments[0], nickName: segments[1], lastName: segments[2] };
+  }
+  else return null;
 }
 
-function getRandomNumber(max){
-  return Math.floor(Math.random() * max);
+function writePageToOutput(response){
+  var options = {
+    root: __dirname + '/public/views'
+  }
+
+  response.sendFile("index.html", options, function(err){
+    if (err){
+      console.log(err);
+    }
+  });
 }
