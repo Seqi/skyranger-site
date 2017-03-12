453cf312c89f5ad22d0d63e78086a0d7b7cd619b
Marcus Crane <marcus@thingsima.de>
2017-03-12 16:28:41 +1300

    Put imports at the top for generate output

--- api/tests/generate-output.js
+++ api/tests/generate-output.js
@@ -1,10 +1,9 @@
+var mocha = require('mocha')
 var testPerson = require('./testData').testPerson
-
-var soldierWriter = require('../src/soldier-writer');
-var newSoldier = soldierWriter(testPerson)
-
-// Read the result back in
 var reader = require('../src/soldier-reader');
+var soldierWriter = require('../src/soldier-writer');
+
+var newSoldier = soldierWriter(testPerson)
 var result = reader(newSoldier).getSoldiers();
 
 console.log(JSON.stringify(result, null, 4));
