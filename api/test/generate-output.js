a26f458ae6d596febb679c1713645cc63c099380
Marcus Crane <marcus@thingsima.de>
2017-03-12 16:36:20 +1300

    Renamed tests to test for mocha and write a first test

--- api/test/generate-output.js
+++ api/test/generate-output.js
@@ -1,9 +1,16 @@
 var mocha = require('mocha')
+var expect = require('chai').expect
 var testPerson = require('./testData').testPerson
 var reader = require('../src/soldier-reader');
 var soldierWriter = require('../src/soldier-writer');
 
 var newSoldier = soldierWriter(testPerson)
 var result = reader(newSoldier).getSoldiers();
+
+describe('Generated soldier object', function () {
+  it('should contain the expected keys', function() {
+    expect(result).to.have.all.keys('soldiers', 'CharacterPool', 'PoolFileName')
+  })
+})
 
 console.log(JSON.stringify(result, null, 4));
