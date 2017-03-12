75c0fce0fb4c124b04406f1290161c3602312c44
Marcus Crane <marcus@thingsima.de>
2017-03-12 16:08:43 +1300

    Removed git merge error

--- api/tests/generate-output.js
+++ api/tests/generate-output.js
@@ -16,19 +16,11 @@ var testPerson = [
   }
 ];
 
-<<<<<<< Updated upstream:api/tests/generate-output.js
 var soldierWriter = require('../src/soldier-writer');
 var newSoldier = soldierWriter(testPerson)
 
 // Read the result back in
 var reader = require('../src/soldier-reader');
-=======
-var soldierWriter = require('../soldier-writer');
-var newSoldier = soldierWriter(testPerson)
-
-// Read the result back in
-var reader = require('../soldier-reader');
->>>>>>> Stashed changes:tests/generate-output.js
 var result = reader(newSoldier).getSoldiers();
 
 console.log(JSON.stringify(result, null, 4));
