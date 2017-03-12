1f27ab5b0a0d5222dfe8e68e5f5671f3669a6330
Marcus Crane <marcus@thingsima.de>
2017-03-12 18:02:59 +1300

    Added property collector test

--- api/test/property-collector.js
+++ api/test/property-collector.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var mocha = require('mocha')
 var expect = require('chai').expect
+var possibleValues = require('./testData').possibleValues
 var reader = require('../src/soldier-reader.js');
 var writer = require('../src/soldier-writer.js');
 
@@ -11,40 +12,47 @@ var files = [
 
 describe('Soldier generator', function() {
 
-// Collect all soldiers in a flat array
-var soldiers = [];
-for (var i = 0; i < files.length; i++){
-  var fileBuffer = fs.readFileSync(files[i]);
-  var readResult = reader(fileBuffer).getSoldiers();
-  soldiers = soldiers.concat(readResult.soldiers);
-}
-
-it('should return the expected number of soldiers', function() {
-  expect(soldiers.length).to.equal(243)
-})
-
-var properties = [];
-var propertiesToIgnore = ["strFirstName", "strLastName", "strNickName", "BackgroundText", "PoolTimestamp"];
-
-// Loop through our sample of soldiers to record all values for each property
-soldiers.forEach(function extractSoldierProperties(soldier){
-  for (var prop in soldier){
-    // Ignore specific properties that are unique or we don't care to record
-    if (prop.type == "BoolProperty" ||
-        propertiesToIgnore.indexOf(prop) >= 0){
-      continue;
-    }
-
-    if (!properties[prop]){
-      properties[prop] = [soldier[prop].val];
-    }
-    // Check the value doesn't exist
-    else if (properties[prop].indexOf(soldier[prop].val) < 0){
-      properties[prop].push(soldier[prop].val);
-    }
+  // Collect all soldiers in a flat array
+  var soldiers = [];
+  for (var i = 0; i < files.length; i++){
+    var fileBuffer = fs.readFileSync(files[i]);
+    var readResult = reader(fileBuffer).getSoldiers();
+    soldiers = soldiers.concat(readResult.soldiers);
   }
-});
 
-}) // Soldier generator test close
+  it('should return the expected number of soldiers', function() {
+    expect(soldiers.length).to.equal(243)
+  })
 
-//console.log(properties);
+  var properties = [];
+  var propertiesToIgnore = ["strFirstName", "strLastName", "strNickName", "BackgroundText", "PoolTimestamp"];
+
+  // Loop through our sample of soldiers to record all values for each property
+  soldiers.forEach(function extractSoldierProperties(soldier){
+    for (var prop in soldier){
+      // Ignore specific properties that are unique or we don't care to record
+      if (prop.type == "BoolProperty" ||
+          propertiesToIgnore.indexOf(prop) >= 0){
+        continue;
+      }
+
+      if (!properties[prop]){
+        properties[prop] = [soldier[prop].val];
+      }
+      // Check the value doesn't exist
+      else if (properties[prop].indexOf(soldier[prop].val) < 0){
+        properties[prop].push(soldier[prop].val);
+      }
+    }
+  });
+
+  it('should contain the expected amount of values', function() {
+    expect(Object.keys(properties)).to.have.lengthOf(48)
+  })
+
+  it('each categories should contain the expected number of values', function() {
+    for (var i of Object.keys(properties)) {
+      expect(properties[i].length).to.equal(possibleValues[i])
+    }
+  })
+})
